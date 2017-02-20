682c645ad200a284b972e7ed1871e2d034d5e8e1
Pehj <peterhj@gmail.com>
2017-02-20 04:45:43 +0100

    fix break

--- src/com/peheje/hiddenMarkov/Main.java
+++ src/com/peheje/hiddenMarkov/Main.java
@@ -3,7 +3,6 @@ package com.peheje.hiddenMarkov;
 import java.util.List;
 
 public class Main {
-someoneBreakCode
   public static void main(String[] args) {
     try {
       String dir = System.getProperty("user.dir");
